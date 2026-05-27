3115f6da65799321d200e2e1fded18fb33719edd
Arkstone27 <mattehaut1427@gmail.com>
2026-05-27 21:48:15 +0200

    rdn aleatoire

--- rdn.py
+++ rdn.py
@@ -1,11 +1,15 @@
 from numba.experimental import jitclass
-from numba import njit, prange, types
+from numba import njit, types
 
 import numpy as np
 
 type_fn_activation = types.FunctionType(types.float32(types.float32))
+type_fn_output = types.FunctionType(types.float32[:](types.float32[:]))
 
 spec = [
+    ("poids", types.float32[:, :, :]),
+    ("biais", types.float32[:, :]),
+
     ("repartition", types.uint8[:]),
     ("nb_inputs", types.uint8),
     ("nb_outputs", types.uint8),
@@ -13,12 +17,24 @@ spec = [
     ("nb_neurones", types.uint8),
     ("nb_hidden", types.uint8),
 
-    ("fn_activation", type_fn_activation)
+    ("fn_activation", type_fn_activation),
+    ("fn_output", type_fn_output),
+    ("derniere_action", types.bool)
 ]
+@njit(types.float32(types.float32))
+def sigmoide(x):
+    return 1 / (1 + np.exp(-x))
+
+@njit(types.float32[:](types.float32[:]))
+def sigmoide_output(output):
+    return 1 / (1 + np.exp(-output))
 
 @jitclass(spec)
 class RdN(object):
-    def __init__(self, repartition, fn_activation):
+    def __init__(self, data_poids, data_biais, repartition, fn_activation, fn_output, derniere_action):
+        self.poids = data_poids
+        self.biais = data_biais
+
         self.repartition = repartition
         self.nb_inputs = repartition[0]
         self.nb_outputs = repartition[1]
@@ -27,11 +43,22 @@ class RdN(object):
         self.nb_hidden = repartition[3] # Nb de couches cachées
 
         self.fn_activation = fn_activation
+        self.fn_output = fn_output
+        self.derniere_action = derniere_action
 
 @njit
-def sigmoide(x):
-    return 1 / (1 + np.exp(-x))
+def rdn_aleatoire(repartition, fn_activation, fn_output, derniere_action):
+    nb_couches = int(repartition[3]) + 1
+    nb_neurones = int(repartition[2])
+
+    data_poids = np.random.rand(nb_couches, nb_neurones, nb_neurones).astype(np.float32) * 2 - 1
+    data_biais = np.random.rand(nb_couches, nb_neurones).astype(np.float32) * 2 - 1
+
+
+    return RdN(data_poids, data_biais, np.array(repartition, dtype=np.uint8), fn_activation, fn_output, derniere_action)
 
 if __name__ == '__main__':
-    repartition = np.array([3,2,3,5], dtype=np.uint8)
-    rdn = RdN(repartition, sigmoide)
+    repartition = (3,2,3,5)
+    rdn = rdn_aleatoire(repartition, sigmoide, sigmoide_output, False)
+
+    print(rdn.poids)
